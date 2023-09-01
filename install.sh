52e6e13d5bb8a8dd43064aaab3aafdd53f3e78f7
Bryce Melander <brycemelander@gmail.com>
2023-09-01 14:58:30 -0700

    Updated

--- install.sh
+++ install.sh
@@ -7,44 +7,60 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+install_nvim_from_source() {
+
+    # Necessary packages
+    declare -a PACKAGES=(
+        gcc
+        ninja-build
+        gettext
+        cmake
+        unzip
+        curl
+    )
+
+    # Prompt
+    echo "Installing the following packages: ${PACKAGES[@]}"
+    # Install packages
+    apt-get update >/dev/null && apt-get install -y ${PACKAGES[@]}
+
+    # Neovim config directory
+    CONFIG_DIR=~/.config/
+
+    # If we're not in the config directory, move there
+    if [[ ! $(pwd) == "$CONFIG_DIR/nvim" ]]; then
+
+        # Make sure the config directory exists
+        mkdir -p $CONFIG_DIR
+
+        # Move everything to the config direcory
+        mv ../nvim $CONFIG_DIR
+    fi
+
+    # Neovim installation directory
+    INSTALL_DIR=/opt/neovim
+
+    # Grab the most recent stable release of Neovim
+    git submodule update --init && git checkout stable
+
+    # Install Neovim!
+    make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR"
+    make install
+
+    # Add Neovim to PATH
+    export PATH="$PATH:$INSTALL_DIR/neovim/bin"
+
+    # Make sure it's working
+    VERSION=$(nvim --version)
+
+    if [[ $? -ne 0 ]]; then
+        echo "Fatal: Neovim returned a non-zero exit status, exiting..."
+        exit 1
+    else
+        echo $VERSION
+    fi
+}
+
+# Begin
 clear
-
-# Necessary packages
-declare -a PACKAGES=(
-    gcc
-    cmake
-    ninja-build
-    gettext
-    cmake
-    unzip
-    curl
-)
-
-# Prompt
-echo "Installing the following packages: ${PACKAGES[@]}"
-# Install packages
-apt-get update >/dev/null && apt-get install -y ${PACKAGES[@]}
-
-# Neovim config directory
-CONFIG_DIR=~/.config/nvim
-
-# Get the current folder location
-PWD=$(pwd)
-
-# If we're not in the config directory, move there
-if [[ ! PWD == CONFIG_DIR ]]; then
-
-    # Make sure the config directory exists
-    mkdir -p ~/.config
-
-    # Move everything to the config direcory
-    mv ../nvim $CONFIG_DIR
-    cd $CONFIG_DIR/install
-
-fi
-
-# Init submodules if necessary
-git submodule update --init
-
-
 
