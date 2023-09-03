4297a77b26fe9d38908cd29d48994c5046ddfb7b
Bryce Melander <brycemelander@gmail.com>
2023-09-02 18:21:05 -0700

    Updated

--- lua/modules/packer.lua
+++ lua/modules/packer.lua
@@ -1,17 +1,20 @@
+local ensure_packer = function()
 
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
--- Install packer if necessary
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    if fn.empty(fn.glob(install_path)) > 0 then
 
-    -- Enable user install message 
-    is_bootstrap = true
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
 
-    -- Clone and install
-    vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-    vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+
+    return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -20,7 +23,7 @@ return require('packer').startup(function(use)
     use {
         'nvim-telescope/telescope.nvim',
         branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = {{'nvim-lua/plenary.nvim'}}
     }
 
     use({
@@ -35,7 +38,7 @@ return require('packer').startup(function(use)
         "folke/trouble.nvim",
         config = function()
             require("trouble").setup {
-                icons = false,
+                icons = false
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
@@ -46,8 +49,8 @@ return require('packer').startup(function(use)
     use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
-            local ts_update = require('nvim-treesitter.install').update({ 
-                with_sync = true 
+            local ts_update = require('nvim-treesitter.install').update({
+                with_sync = true
             })
             ts_update()
         end
@@ -63,24 +66,13 @@ return require('packer').startup(function(use)
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+        requires = { -- LSP Support
+        {'neovim/nvim-lspconfig'}, {'williamboman/mason.nvim'}, {'williamboman/mason-lspconfig.nvim'},
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'}, {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-nvim-lua'}, -- Snippets
+        {'L3MON4D3/LuaSnip'}, {'rafamadriz/friendly-snippets'}}
     }
 
     use("folke/zen-mode.nvim")
@@ -88,16 +80,8 @@ return require('packer').startup(function(use)
     use("eandrju/cellular-automaton.nvim")
     use("laytan/cloak.nvim")
 
-    -- Start package installation and prompt user to restart once complete 
-    if is_bootstrap then
+    -- Automatically set up your configuration after cloning packer.nvim
+    if packer_bootstrap then
         require('packer').sync()
-
-        print '=================================='
-        print '    Plugins are being installed'
-        print '    Wait until Packer completes,'
-        print '       then restart nvim'
-        print '=================================='
-
     end
-
 end)
